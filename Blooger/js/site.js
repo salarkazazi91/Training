4751910998714945136488b81ac4591a7d6f0f5d
salarkazazi91 <74836030+salarkazazi91@users.noreply.github.com>
2020-11-22 18:39:26 +0330

    Add files via upload

--- Blooger/js/site.js
+++ Blooger/js/site.js
@@ -1,8 +1,34 @@
-$(document).ready(function () {
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+$(document).ready(function() {
 
-    $('.toggle-btn').click(function () {
+    $('.toggle-btn > a > i').click(function() {
         $('.nav').toggleClass('collapse');
     });
 
 
+
+
+    //owl-carousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
 });
